e2e5950c956f6aee5ef38082f6aca4c4b24a6646
artem <artemsh@mmvs.ru>
2020-01-08 02:10:28 +0500

    Added work with files and methods

--- MyWindow.py
+++ MyWindow.py
@@ -1,24 +1,59 @@
+import os
+
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 
 class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MyWindow,self).__init__()
         uic.loadUi('SlopeForse.ui', self)
+        self.initUI()
 
+    def initUI(self):
+        self.setWindowTitle('Поверхность скольжения')
 
-    def example(self):
-        pass
+        self.pushButton_calculate.clicked.connect(self.calculation)
+        self.pushButton_calculate.clicked.connect(self.calculation)
+
+        self.pushButton_calculate.clicked.connect(self.calculation)
+        self.pushButton_show_result.clicked.connect(self.showResult)
+        self.pushButton_save_result.clicked.connect(self.saveFile)
+        # self.openFile()
+
     def calculation(self):
         pass
 
+    def showResult(self):
+        pass
+
+    def saveFile(self, text):
+        name = QtWidgets.QFileDialog.getSaveFileName(self, 'Сохранить файл')
+        file = open(name, 'w')
+        file.write(text)
+        file.close()
+
+    def setProgress(self, minValue, maxValue, value):
+        self.progressBar.setValue(value)
+        self.progressBar.setMaximum(maxValue)
+        self.progressBar.setMinimum(minValue)
+
+    def openFile(self):
+        name = QtWidgets.QFileDialog.getOpenFileName(self, "Выберите файл", os.getcwd(), "txt (*.txt)")
+        file = open(name, 'r')
+        with file:
+            text = file.read()
+            print(text)
 
 
-if __name__ == '__main__':
+def main():
     import sys
     app = QtWidgets.QApplication(sys.argv)
     window = MyWindow()
+
     window.show()
     sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
 
     # from PyQt5 import QtWidgets, QtGui
     # from qtLabel import Ui_MainWindow
